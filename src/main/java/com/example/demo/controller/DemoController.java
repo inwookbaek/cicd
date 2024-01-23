3b2f1a23abf38fec3c136f33252e1cfa5f584069
inwookbaek <iwbaek@gmail.com>
2024-01-23 12:42:00 +0900

    4th commit

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,13 +1,16 @@
 package com.example.demo.controller;
 
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
     @GetMapping("/")
+    @ResponseBody
     public String hello() {
-        return "{'id':'gilbaek', 'name':'gilbaek'}";
+        return "{'id':'gilbaek', 'name':'백인욱'}";
     }
 }
